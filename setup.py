7352659782e691c599f2c9dce20aa8ece5761aea
danielto1404 <daniilkorolev00@gmail.com>
2022-12-03 22:59:06 +0300

    up version 0.0.2

--- setup.py
+++ setup.py
@@ -1,29 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vision-transformer',
+    name="vision-transformer",
     packages=find_packages(exclude=[]),
-    version='0.0.1',
-    license='MIT',
-    description='PyTorch implementation of Vision Transformer paper: https://arxiv.org/abs/2010.11929',
-    author='Daniil Korolev',
-    url='https://github.com/Danielto1404/vision-transformer',
+    version="0.0.2",
+    license="MIT",
+    description="PyTorch implementation of Vision Transformer paper: https://arxiv.org/abs/2010.11929",
+    author="Daniil Korolev",
+    url="https://github.com/Danielto1404/vision-transformer",
     keywords=[
-        'Artificial Intelligence',
-        'OpenAI gym',
-        'Deep Learning',
-        'Reinforcement Learning',
-        'Trading environment',
-        'Crypto'
+        "Vision Transformer"
+        "Artificial Intelligence",
+        "Deep Learning",
+        "Computer Vision",
+        "PyTorch",
+        "Attention Is All You Need"
     ],
     install_requires=[
         "torch",
         "einops"
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Programming Language :: Python :: 3.9',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Programming Language :: Python :: 3.9",
     ],
 )
